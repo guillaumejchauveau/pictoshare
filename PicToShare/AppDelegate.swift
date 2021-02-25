519bd860b9470b9cd1aac5475222e04b7cb54a32
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2021-02-25 17:08:12 +0100

    Add initial Core structure, demo Standard Library

--- PicToShare/AppDelegate.swift
+++ PicToShare/AppDelegate.swift
@@ -2,43 +2,18 @@
 //  AppDelegate.swift
 //  PicToShare
 //
-//  Created by Guillaume Chauveau on 28/01/2021.
-//
+//  Created by Guillaume Chauveau on 25/02/2021.
 //
 
-import Cocoa
 import SwiftUI
 
-
-@main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    var window: NSWindow!
-
-
+    let libraryManager = LibraryManager()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Create the SwiftUI view that provides the window contents.
-    let contentView = ContentView()
+        try! libraryManager.load(library: StandardLibrary())
 
-
-    // Create the window and set the content view.
-    window = NSWindow(
-        contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-        styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-        backing: .buffered, defer: false)
-    window.isReleasedWhenClosed = false
-    window.center()
-    window.setFrameAutosaveName("Main Window")
-    window.contentView = NSHostingView(rootView: contentView)
-    window.makeKeyAndOrderFront(nil)
+        let source = libraryManager.get(source: "standard.sources.filesystem", with: ["name": "fs"])
+        source.promptForDocument(with: [:])
     }
-
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-    // Insert code here to tear down your application
-    }
-
-
-
 }
